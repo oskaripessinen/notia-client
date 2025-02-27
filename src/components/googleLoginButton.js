b55d3c35672edbc3d9f78655c3a797e47a48df2a
oskaripessinen <oskari.pessinen@gmail.com>
2025-02-28 01:03:32 +0200

    client commit - 1 files changed in src

--- src/components/googleLoginButton.js
+++ src/components/googleLoginButton.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { useNavigate, useEffect } from "react-router-dom";
+import React, {useEffect} from "react";
+import { useNavigate } from "react-router-dom";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import AuthService from "../services/authService";
 
